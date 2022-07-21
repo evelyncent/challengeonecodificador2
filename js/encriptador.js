124b7c1cbd7e2de195d9267c47c060073d4fdbfa
evelyncent <evelyncent@hotmail.com>
2022-07-21 16:51:36 -0300

    js

---
+++ js/encriptador.js
@@ -0,0 +1,7 @@
+const inputMensaje=document.querySelector("#mensaje");
+const inputResultado=document.querySelector("#resultado");
+const btnEncriptar=docuemnte.querySelector("#encriptar");
+const btnDesencriptar=docuemnte.querySelector("#desencriptar");
+const btnCopiar=document.querySelector("#copiar");
+const btnEscuchar=document.querySelector("#escuchar");
+const contenedorErrores=document.querySelector(".contenedor-errores");
