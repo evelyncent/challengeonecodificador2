9ad3ade8f4b93d64e62039789290442df3c98854
evelyncent <evelyncent@hotmail.com>
2022-07-21 17:38:54 -0300

    desarrollando js

--- js/encriptador.js
+++ js/encriptador.js
@@ -5,3 +5,60 @@ const btnDesencriptar=docuemnte.querySelector("#desencriptar");
 const btnCopiar=document.querySelector("#copiar");
 const btnEscuchar=document.querySelector("#escuchar");
 const contenedorErrores=document.querySelector(".contenedor-errores");
+
+
+function validarMensajes(){
+    let erroresPrevios=contenedorErrores.querySelectorAll(".error");
+    for (let err of erroresPrevios){
+
+        contenedorErrores.removeChild(err);
+    }
+    var mensaje=inputMensaje.ariaValueMax;
+    let letrasValidas="abcdefghijklmnopqrstuvwxyz";
+    let mensajeError=document.createDocumentFragment();
+    for(let letra of mensaje)
+{if (!letrasValidas.includes(letra)){
+    let p =document.createElement("p");
+    p.setAttribute("class","error");
+    p.textContent=`La letra ${letra} no es valida`;
+    mensajeEror.appendChild(p);
+}
+}
+contenedorErrores.append(mensajeError);
+if(contenedorErrores.children.length===0){
+    return true;
+}
+return false;
+}
+function encriptar(){
+    if (!validarMensajes())return;
+    var mensaje=inputMensaje.ariaValueMax;
+    var mensajeEncriptado=mensaje
+    .replaceAll("e","enter")
+    .replaceAll("imes","i")
+    .replaceAll("ober","o")
+    .replaceAll("ai","a")
+    .replaceAll("ufat","u");
+    inputResultado.value=mensaje;
+
+}
+
+function copiar (){
+    var mensajeEncriptado=inputResultado.value;
+    navigator.clipboard.writeText(mensajeEncriptado);
+    inputMensaje.value="";
+    inputMensaje.focus();
+}
+
+function escuchar (){
+    var mensajeEncriptado=inputResultado.value;
+    let msg=new SpeechSynthesisUtterance();
+    msg.text=mensajeEncriptado;
+    msg.lang="es-ES";
+    Window.SpeechSynthesis.speak(msg);
+}
+
+btnEncriptar.onclick=encriptar;
+btnDesencriptar.onclick=desencriptar;
+btnCopiar.onclick=copiar;
+btnEscuchar.onclick=escuchar;
